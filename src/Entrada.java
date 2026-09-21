dc987a2c8b10379000633bdc81c178ea071ac79e
AlejandroMolinaDeHaro <alexmolinadeharo@gmail.com>
2026-09-21 17:15:49 +0200

    ahora parece que si

---
+++ src/Entrada.java
@@ -0,0 +1,27 @@
+/**
+ * @author Aljandro
+ * @version 1.0
+ */
+public class Entrada {
+
+    // aqui explicas brevemente para aclarar ideas
+    // TODO esta tarea pendiente para el martes
+    // aqui explicas brevemente para aclarar ideas
+    /*
+    Este comentario te admite unas cuantas lineas
+    Esta es la segunda linea
+     */
+
+
+
+    public static void main(String[] args){
+        System.out.print("hola mundo");
+    }
+    /**
+     * @param arg explica el parametro
+     * @return explica el restart
+     */
+    public int metodo(String arg){
+        return 1;
+    }
+}
