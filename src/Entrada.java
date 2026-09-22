70dc3604ce1e7aaa463267e3d3eb9662bd763ee8
AlejandroMolinaDeHaro <alexmolinadeharo@gmail.com>
2026-09-22 16:55:37 +0200

    Nueva clase

--- src/Entrada.java
+++ src/Entrada.java
@@ -6,22 +6,42 @@ public class Entrada {
 
     // aqui explicas brevemente para aclarar ideas
     // TODO esta tarea pendiente para el martes
-    // aqui explicas brevemente para aclarar ideas
-    /*
-    Este comentario te admite unas cuantas lineas
-    Esta es la segunda linea
-     */
 
-
-
+    // mod_acceso retorno nombre (args){ funcionalidad }
     public static void main(String[] args){
-        System.out.print("hola mundo");
+       //ordenes a ejecutar
+        // atajo es sout
+        // para saltar linea es \n
+        //segun el dato que guarda: String, char, byte/shot/int/long, double/float,
+        //tipo nombre = valor
+
+        String nombreLegal = "Alejandro";
+        String nombre = "null";
+        char letra = 'r';
+        int edad = 19;
+        double altura = 1.70;
+        boolean acierto = false;
+
+        System.out.println("Mi nombre es "+nombreLegal);
+        System.out.println("La letra de mi DNI es "+letra);
+        System.out.println("Mi edad es "+edad);
+        System.out.println("Mi altura es "+altura);
+        System.out.println("El resultado de la evaluacion es "+acierto);
+        System.out.println("hola mundo");
+        System.out.println(9*6);
+        System.out.println(true);
+        System.out.println(10.10);
+        System.out.println("segunda linea");
+        System.out.println(nombre.length());
+        System.out.println("\tcuarta linea\n");
+        //la suma de 9 y 6 es 15
+        System.out.println("la suma de "+9+" y "+6+" tiene como resultado "+ (9+6));
     }
+
+
     /**
      * @param arg explica el parametro
      * @return explica el restart
      */
-    public int metodo(String arg){
-        return 1;
-    }
+    public int metodo(String arg){ return 1;}
 }
